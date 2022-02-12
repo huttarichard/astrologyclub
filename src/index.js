6a7959c52902cf968404641fdbc21feb26c6bba3
Michał Bartczak <probartczak@gmail.com>
2022-02-12 23:35:24 +0100

    prepend root element in right place

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const rootNode = document.createElement('div');
+rootNode.id = 'root'
+
+document.querySelector('.header-display-desktop .header-actions').prepend(rootNode)
+
 const web3 = new Web3(
   Web3.givenProvider || 'wss://main-light.eth.linkpool.io/ws'
 );
